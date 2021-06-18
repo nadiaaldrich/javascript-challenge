92b7a0072b00fe68ab369a2420b24b60812e4da7
N Aldrich <naldrich@hs-27306.local>
2021-06-18 09:59:05 -0500

    saving

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,5 +6,17 @@ const tbody = d3.select("tbody");
 
 function buildTable(data) {
     tbody.html("");
+
+
+    // loop through data and append rows and cells
+    data.forEach(dataRow) => {
+        const row = tbody.append("tr");
+
+        // loop through each field in the dataRow and add value as cell
+        Object.values(dataRow).forEach((val)) => {
+            let cell = row.append("td");
+                cell.text(val);
+            }
+        );
+
     
-}
